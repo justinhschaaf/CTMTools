f511d3616f1c155a1c7df00acbca5f87018ca2e7
Justin Schaaf <justinhschaaf@outlook.com>
2019-07-06 22:59:44 -0700

    Begin adding site content
    - Started adding site content
    - Changed the prefix to "ctm" and the outputPath to "docs"
    - Added build.bat and CNAME_STORAGE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
